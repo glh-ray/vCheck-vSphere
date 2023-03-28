5e1fcc028d80d6bc08b5d9d780990c670eca66ff
glh-ray <128434439+glh-ray@users.noreply.github.com>
2023-03-28 16:35:53 -0700

    Update 28 Removable Media Connected.ps1
    The method implemented to ignore specific VMs is malformed and ignoring all VMs.  This is a small change to correct that on line 18.

--- Plugins/60 VM/28 Removable Media Connected.ps1
+++ Plugins/60 VM/28 Removable Media Connected.ps1
@@ -2,19 +2,20 @@ $Title = "Removable Media Connected"
 $Header = "VMs with Removable Media Connected: [count]"
 $Comments = "The following VMs have removable media connected (i.e. CD/Floppy), this may cause issues if this machine needs to be migrated to a different host"
 $Display = "Table"
-$Author = "Alan Renouf, Frederic Martin"
-$PluginVersion = 1.1
+$Author = "Alan Renouf, Frederic Martin, Gaven Henderson"
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"
 
 # Start of Settings 
 # VMs with removable media not to report on
+# $IgnoreVMMedia = "vm1","vm2"
 $IgnoreVMMedia = ""
 # End of Settings
 
 # Update settings where there is an override
 $IgnoreVMMedia = Get-vCheckSetting $Title "IgnoreVMMedia" $IgnoreVMMedia
 
-$FullVM | Where-Object {$_.runtime.powerState -eq "PoweredOn" -And $_.Name -notmatch $IgnoreVMMedia} | 
+$FullVM | Where-Object {$_.runtime.powerState -eq "PoweredOn" -And $IgnoreVMMedia -notcontains $_.Name} | 
    % { $VMName = $_.Name; $_.config.hardware.device | Where-Object {($_ -is [VMware.Vim.VirtualFloppy] -or $_ -is [VMware.Vim.VirtualCdrom]) -and $_.Connectable.Connected} | 
       Select-Object @{Name="VMName"; Expression={ $VMName}}, 
              @{Name="Device Type"; Expression={ $_.GetType().Name}},
@@ -25,3 +26,4 @@ $FullVM | Where-Object {$_.runtime.powerState -eq "PoweredOn" -And $_.Name -notm
 # Change Log
 ## 1.0 : Initial release
 ## 1.1 : Added Get-vCheckSetting
+## 1.2 : Fixed ignore
